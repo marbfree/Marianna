99e064588cc8feb58b6bde2955886d4c2b3fa7ec
Disco Aces <129333044+marbfree@users.noreply.github.com>
2024-04-09 06:05:28 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavTabs from './components/layout/Header'; 
 import Home from './components/pages/Home';
 
 function App() {
@@ -7,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Home></Home>
-        <NavTabs></NavTabs>
       </header>
     </div>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from '../pages/styles.module.css';
+import NavTabs from '../layout/Header';
 
 const Home = () => {
     return (
         <div>
         <header className={styles.background}>
-        <h1 className={styles.fadeInHello}>Marianna Bruns</h1> 
-        
+        <h1 className={styles.fadeInHello}>Marianna</h1> 
+        <NavTabs></NavTabs>
         </header>
         </div>
     )
