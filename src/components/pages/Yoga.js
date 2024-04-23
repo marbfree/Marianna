33b7f01d80e1835177f3d7a1b2cf902bea7ae109
Disco Aces <129333044+marbfree@users.noreply.github.com>
2024-04-22 20:53:17 -0600

    added yoga background

--- src/components/pages/Yoga.js
+++ src/components/pages/Yoga.js
@@ -4,7 +4,7 @@ import styles from "./styles.module.css"
 export default function Yoga() {
     return (
         <body>
-            <div className={styles.background}>
+            <div className={styles.backgroundYoga}>
                 <h2 className={styles.fadeInHello}>Yoga and Meditation</h2>
             </div>
         </body>
